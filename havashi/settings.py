8454655043820bc08af947559b7026b6c9582291
Ali Tayebi <42505961+alitayebi@users.noreply.github.com>
2022-05-27 05:10:11 +0430

    remove psycopg2

--- havashi/settings.py
+++ havashi/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import os.path, psycopg2, dj_database_url
+import os.path, dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
